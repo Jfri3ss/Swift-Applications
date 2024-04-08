bf872a0711c3e5c099d15e83aa0064859122ee0d
Jeremiah Fries <77308708+Jfri3ss@users.noreply.github.com>
2024-04-08 11:10:19 -0400

    Create Questions.swift

---
+++ Questions.swift
@@ -0,0 +1,30 @@
+//
+//  Questions.swift
+//  Trivia Game
+//
+//  Created by Steve Jobs on 4/8/24.
+//
+
+import Foundation
+
+//data model struct
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
