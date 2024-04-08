605ccc13c7730b05000b9ec5c450c7ab7d9b2669
Jeremiah Fries <77308708+Jfri3ss@users.noreply.github.com>
2024-04-08 11:09:19 -0400

    Create Trivia_GameApp.swift

---
+++ Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Jeremiah on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
