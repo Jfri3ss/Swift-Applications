efe40f3a24d0b311d1ec5ddedaf9ed8f7be02e64
Jeremiah Fries <77308708+Jfri3ss@users.noreply.github.com>
2024-04-23 13:31:07 -0400

    Update QuestionView.swift

--- QuestionView.swift
+++ QuestionView.swift
@@ -1,14 +1,14 @@
 //
 //  QuestionView.swift
-//  Trivia Game
+//  CodeHistory
 //
-//  Created by Steve Jobs on 4/23/24.
+//  Created by J Fries on 3/4/21.
 //
 
 import SwiftUI
 
 struct QuestionView: View {
-    
+    @EnvironmentObject var viewModel: GameViewModel
     let question: Question
     
     var body: some View {
@@ -19,26 +19,29 @@ struct QuestionView: View {
                 .multilineTextAlignment(.leading)
             Spacer()
             HStack {
-                ForEach(0..<question.possibleAnswers.count) { answerIndex in
-                    Button(action: {
-                        print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
-                        
-                    }) {
-                        ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
-                        
+                ForEach(0..<question.possibleAnswers.count) { index in
+                    Button {
+                        viewModel.makeGuess(atIndex: index)
+                        print("Tapped on option with the text: \(question.possibleAnswers[index])")
+                    } label: {
+                        ChoiceTextView(choiceText: question.possibleAnswers[index])
+                            .background(viewModel.color(forOptionIndex: index))
                     }
+                    .disabled(viewModel.guessWasMade)
                 }
-                
+            }
+            if viewModel.guessWasMade {
+                Button(action: { viewModel.displayNextScreen() }) {
+                    BottomTextView(str: "Next")
+                }
             }
         }
     }
 }
 
-
-
-
-
-
-
-
-
+struct QuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView(question: Game().currentQuestion)
+            .environmentObject(GameViewModel())
+    }
+}
